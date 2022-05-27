d8deb58cf48958967b4f617f490ba846fa9a36b7
SolangeRSC <solangersc.26@gmail.com>
2022-05-27 12:07:49 +0100

    starting project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary Project</header>
+    </div>
+  );
+}
+
+export default App;
