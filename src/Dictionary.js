5754818bec07dee9985e67ac90eb10a3f48283fd
SolangeRSC <solangersc.26@gmail.com>
2022-05-28 17:15:44 +0100

    created results.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState({});
   function search(event) {
     event.preventDefault();
   }
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
   let urlApi = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(urlApi).then(handleResponse);
@@ -20,6 +23,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeyword}></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,6 @@
+import React from "react";
+import axios from "axios";
+
+export default function Results(props) {
+  console.log(props.results);
+}
